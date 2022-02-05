1221efee0e221be2d0998057bb620e9ec511a827
r0xANDt0l <rodrigosgdlcc@gmail.com>
2022-02-05 16:15:51 +0100

    Sustitution Chars

--- FirstProject/main.c
+++ FirstProject/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
+    // %s -> Strings
+    // %d -> Whole numbers (1, 2, 3...)
+    // %f -> floating point numbers (floats) (3.141592...)
+    // %.xf -> x Round floating point numbers to the value of x ( "I am %.2f human" -> "I am 99.17 human" BUT NOT "I am 99,99999 human")
 
-    printf("Las olimipiadas de Beijing\a"); // First commend yay
-    printf("I'm cool :swag:");
+    printf("Pi is: %.2f\n", 3.141592);
+    printf("Pi is: %.1f\n", 3.141592);
+    printf("Pi is: %.5f\n", 3.141592);
 
     return 0;
 }
