291b7d3d5fc5bf3641b16223f6f8c6aedf4e997e
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-06-27 16:22:53 +0200

    clion stufff

---
+++ cmake-build-debug/CMakeFiles/C_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Learning.dir/FirstProject/main.c.o"
+  "CMakeFiles/C_Learning.dir/FirstProject/main.c.o.d"
+  "C_Learning"
+  "C_Learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
